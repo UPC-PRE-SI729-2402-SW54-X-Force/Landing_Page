7d9ee7351412a149a1fbee754ac18c31e7399130
GeorgeGC23 <gc_oso123@hotmail.com>
2024-09-25 16:21:31 -0500

    feat: added how it works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
-import { WhyChooseUsComponent } from './why-choose-us/why-choose-us.component'; // Importamos el nuevo componente
+import { WhyChooseUsComponent } from './why-choose-us/why-choose-us.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component'; // Importamos el nuevo componente
 
 export const routes: Routes = [
   { path: '', component: HeroComponent }, // Ruta para HeroComponent en la raíz
   { path: 'why-choose-us', component: WhyChooseUsComponent }, // Ruta para WhyChooseUsComponent
+  { path: 'how-it-works', component: HowItWorksComponent }, // Ruta para HowItWorksComponent
 
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' } // Redireccionar cualquier ruta no encontrada a la raíz
 ];
