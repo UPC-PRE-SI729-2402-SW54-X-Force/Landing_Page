e88ddadcc2bb910ee95f9d0703f678934dc67bc6
GeorgeGC23 <gc_oso123@hotmail.com>
2024-09-25 16:11:44 -0500

    feat: added why-choose-us

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { WhyChooseUsComponent } from './why-choose-us/why-choose-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule, CommonModule, NavbarComponent, HeroComponent]
+  imports: [RouterModule, CommonModule, NavbarComponent, HeroComponent, WhyChooseUsComponent]
 })
 export class AppComponent {
   title = 'LandingPage';
